87891d77dbb71b0893d7da5cb0ce9af7ac6600c6
Daniel Wegener <daniel.wegener@holisticon.de>
2014-01-09 20:17:21 +0100

    fixed sbt build

--- project/Build.scala
+++ project/Build.scala
@@ -7,15 +7,20 @@ object Build extends Build {
   import Dependencies._
 
 
-  lazy val root = Project(id = "spray-showcase", base = file("."))
-    .aggregate(`spray-http-example`, `spray-routing-example`)
+  lazy val root = project.in(file("."))
+    .aggregate(`spray-http-example`, `spray-routing-example`, slides)
 
-  lazy val `spray-http-example` = Project(id = "spray-http-example", base = file("spray-http-example"))
+  lazy val `spray-http-example` = project
     .settings(formatSettings: _*)
     .settings(libraryDependencies ++= sprayWithAkkaDependencies)
 
-  lazy val `spray-routing-example` = Project(id = "spray-routing-example", base = file("spray-routing-example"))
+  lazy val `spray-routing-example` = project
     .settings(formatSettings: _*)
     .settings(libraryDependencies ++= sprayWithAkkaDependencies)
+
+  lazy val slides = project
+    //todo: add grunt plugin
+    //.settings(SbtGruntTask.gruntTaskSettings)
+
 
 }
